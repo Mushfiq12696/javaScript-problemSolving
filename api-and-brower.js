0e2fb36faef77704ca0ab0ffd91cccd561e1929f
Mushfiq12696 <musfiqur15-12696@diu.edu.bd>
2023-05-05 14:22:40 +0600

    js for react

--- api-and-brower.js
+++ api-and-brower.js
@@ -6,4 +6,22 @@ const person = {
 const data = (JSON.stringify(person));
 
 const dataParsed = JSON.parse(data);
-console.log(dataParsed);
+// console.log(dataParsed.name);
+
+
+//local Storage
+
+// localStorage.setItem('useId', 1245);
+// console.log(localStorage.getItem(userId));
+const useId = localStorage.getItem('useId');
+// console.log(useId);
+
+localStorage.setItem('person', JSON.stringify(person));
+const storedPerson = localStorage.getItem('person');
+const parsedPerson = JSON.parse(storedPerson);
+// console.log(parsedPerson.power);
+
+const keys = Object.keys(parsedPerson);
+console.log(keys);
+const values = Object.values(parsedPerson);
+console.log(values);
